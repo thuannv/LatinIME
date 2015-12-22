b5a572db2de58c377b3ab552adc84dabf3ade27d
Thuan Nguyen Van <nguyenvanthuan.vnit@gmail.com>
2015-12-22 13:53:32 +0700

    Fixed dependencies

--- java/src/com/android/inputmethod/dictionarypack/MetadataDbHelper.java
+++ java/src/com/android/inputmethod/dictionarypack/MetadataDbHelper.java
@@ -34,7 +34,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeMap;
 
-import javax.annotation.Nullable;
+import android.support.annotation.Nullable;
 
 /**
  * Various helper functions for the state database
